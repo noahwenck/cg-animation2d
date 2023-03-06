32bcf8114017d21242c76363911be0d7ecc17ee4
Lennart Buhl <69181932+Lennart2001@users.noreply.github.com>
2023-03-06 10:48:28 -0600

    Slide3 v0.2 Figured out the Scaling... really weird

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -16,6 +16,23 @@ class Renderer {
         this.s1theta = 0;   // Theta for First Spinning Polygon
         this.s2theta = 0;   // Theta for Second Spinning Polygon
         this.s3theta = 0;   // Theta for Third Spinning Polygon
+
+
+        // Values for Polygon_1 for Slide2 (3rd Slide)
+        this.square_slide_2 = [
+            new Matrix(3, 1),
+            new Matrix(3, 1),
+            new Matrix(3, 1),
+            new Matrix(3, 1)
+        ];
+        this.square_slide_2[0].values = [175, 250, 1]; // Right | Down
+        this.square_slide_2[1].values = [100, 330, 1]; // Left  | Down
+        this.square_slide_2[2].values = [160, 310, 1]; // Left  | Up
+        this.square_slide_2[3].values = [250, 350, 1]; // Right | Up
+
+
+
+
     }
 
     // flag:  bool
@@ -269,37 +286,114 @@ class Renderer {
         // Yes I looked up wtf a polygon is - dont come at me
 
         // TODO Create Scaling thingy which pushes thing to right/left - up/down
-        let trans = new Matrix(3, 3);
-        trans.values = mat3x3Translate(mat3x3Identity, 200, 200);
-        let trans2 = new Matrix(3, 3);
-        trans2.values = mat3x3Translate(mat3x3Identity, -300, -300);
-        let rotate = new Matrix(3, 3);
-        rotate.values = mat3x3Rotate(mat3x3Identity, this.s1theta);
-        let schange = new Matrix(3, 3);
-        schange = Matrix.multiply([trans, rotate, trans2]);
+        let right = new Matrix(3, 3);
+        right.values = mat3x3Scale(mat3x3Identity, 1.1, 1); // Shifts RIGHT TRUE
+        let left = new Matrix(3, 3);
+        left.values = mat3x3Scale(mat3x3Identity, 0.9, 1); // Shifts LEFT TRUE
+
+        let up = new Matrix(3, 3);
+        up.values = mat3x3Scale(mat3x3Identity, 1, 1.1); // Shifts UP TRUE
+        let down = new Matrix(3, 3);
+        down.values = mat3x3Scale(mat3x3Identity, 1, 0.9); // Shifts DOWN TRUE
 
 
 
-        let square = [
-            new Matrix(3, 1),
-            new Matrix(3, 1),
-            new Matrix(3, 1),
-            new Matrix(3, 1)
-        ];
+        // Draw Square before modification to compare difference after modification
 
-        square[0].values = [175, 250, 1]; // Right | Down
-        square[1].values = [100, 330, 1]; // Left  | Down
-        square[2].values = [160, 310, 1]; // Left  | Up
-        square[3].values = [250, 350, 1]; // Right | Up
+//        for (let x = 0; x < 2; x++) {
+//            square[x] = Matrix.multiply([this.move_point_left(random_num), square[x]]);
+//        }
 
+//         let square = [
+//            new Matrix(3, 1),
+//            new Matrix(3, 1),
+//            new Matrix(3, 1),
+//            new Matrix(3, 1)
+//        ];
 
-        for (let x = 0; x < 4; x++) {
-            square[x] = Matrix.multiply([schange, square[x]]);
-        }
-        this.drawConvexPolygon(square, [255, 128, 128, 255]);
+//        this.drawConvexPolygon(this.square, [128, 128, 255, 255]);
 
 
+        // Move This LEFT
+        let random_num = Math.floor(Math.random() + 0.5);
+        let temp = new Matrix(3, 3);
+        temp.values = mat3x3Scale(mat3x3Identity, 0.99 + (0.005 * random_num), 1);
+        this.square_slide_2[0] = Matrix.multiply([temp, this.square_slide_2[0]]);
+        this.square_slide_2[1] = Matrix.multiply([temp, this.square_slide_2[1]]);
 
+        // Move This DOWN
+        random_num = Math.floor(Math.random() + 0.5);
+        temp.values = mat3x3Scale(mat3x3Identity, 1, 0.99 + (0.005 * random_num));
+        this.square_slide_2[2] = Matrix.multiply([temp, this.square_slide_2[2]]);
+        this.square_slide_2[3] = Matrix.multiply([temp, this.square_slide_2[3]]);
+
+
+        this.drawConvexPolygon(this.square_slide_2, [255, 128, 128, 128]);
+
+
+        // TODO Create Algorithms for Updating of Triangles -> Left/Right + Up/Down
+        /*
+
+        Algorithm Left/Right:
+        - Make sure we slowly incriment the scale *NO MULTIPLICATION* Only Addition
+        - - Problem 1:
+        - - - We Could Use Multiplication if we Use Matrix as feature in function -> Input
+        - - - This would Allow us to Just RETURN whatever the correct values would be and the we
+        - - - set the actual values of the Matrix to the CORRECT Values
+        - - - Problems with this -> Could be SLOW as well as lots of code.
+        - - Solution 1:
+        - - - Slightly incrementing values by 0.01 (OR 0.005) with each FRAME
+        - - - - How would we UPDATE? Need global Variable?? Would Assume so.
+        - - - - OR We just say "Fuck it, we ball" and just create function and see what happens
+        - - - - - I Vote for this - I am the only one voting - I win
+        - - - Additionally: ADD Some RANDOMNESS to the Whole process
+        - - - - Switch between Update of 0.01 to 0.005 or something -> Not too Uniform
+        - - Final Solution:
+        - - - Simply create functions
+        - - - We might need Four functions
+        - - - - Depending on Where Which point is, we change the type of Function being Called
+        - - - - - This is because points move LEFT AND RIGHT -> Therefore Utilize both functions
+
+        TODO Look Below - Line 324
+        1) Create Left/Right Functions
+        2) Add Randomization (0.01 / 0.005)
+
+
+
+        */
+
+
+    }
+
+// TODO We PROBABLY won't need these functions
+    // Manually Added Function For Moving Point to the Left
+    move_point_left(random_num, vect, scaler) {
+        let temp = new Matrix(3, 1);
+        vect[0] = vect[0] - 1 + (scaler * random_num);
+        temp.values = vect;
+        return temp.values; // Shifts LEFT
+    }
+
+    move_point_right(random_num, vect, scaler) {
+        let temp = new Matrix(3, 3);
+        temp.values = mat3x3Scale(mat3x3Identity, 1.1, 1);
+        vect = Matrix.multiply([mat3x3Scale(mat3x3Identity, 1.1, 1), vect])
+        return vect; // Shifts RIGHT
+    }
+
+    move_point_up(random_num, vect, scaler) {
+        let temp = new Matrix(3, 1);
+        console.log(vect);
+        vect[1] = vect[1] + 1 + (scaler * random_num);
+        temp.values = vect;
+        return temp.values; // Shifts UP
+    }
+
+    move_point_down(random_num, vect, scaler) {
+        let temp = new Matrix(3, 1);
+        vect[1] -= 1 + (scaler * random_num);
+        temp.values = vect;
+        return temp.values; // Shifts DOWN
     }
 
     //
